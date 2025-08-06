a910b71575c1d90438f393a8feb39e85c97db705
Jatupon Itsara <jatuponitsara@Jatupons-MacBook-Pro.local>
2025-08-06 14:49:08 +0700

    first commit

---
+++ datas/about.js
@@ -0,0 +1,13 @@
+const aboutData = {
+  name: "Jatupon Itsara",
+  description: "I am currently a Bachelor degree's student of Assumption University",
+
+};
+
+window.doingList = {
+  doing: [
+   "Play trombone",
+    "Make the Roblox game",
+    "Make the game from Java"
+  ]
+};
---
+++ datas/script.js
@@ -0,0 +1,11 @@
+// hamburger menu
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("show");
+}
+
+document.querySelectorAll('#menu a').forEach(link =>
+    link.addEventListener('click', () => {
+        document.getElementById("menu").classList.remove("show");
+    })
+);s
